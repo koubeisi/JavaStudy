90d122212b68e55561e975382628e722f276ba54
koubeisi <koubeisi@qq.com>
2025-05-16 20:47:56 +0800

    perf(thread): 比较 Synchronized、AtomicInteger 和 LongAdder 的性能
    - 在 SynchronizedTest 类中添加了 AtomicInteger 和 LongAdder 实例
    - 实现了三种不同同步机制的性能测试- 使用 CountDownLatch 优化了线程同步逻辑
    - 移除了原有的 while 循环等待线程结束的方式

--- thread/src/main/java/com/koubs/thread/keywords/sync/lock/SynchronizedTest.java
+++ thread/src/main/java/com/koubs/thread/keywords/sync/lock/SynchronizedTest.java
@@ -1,5 +1,9 @@
 package com.koubs.thread.keywords.sync.lock;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
+
 /**
  * @author KouBeisi
  * @since 2025/5/12
@@ -12,18 +16,51 @@ public class SynchronizedTest {
         i++;
     }
 
-    public static void main(String[] args) {
+    private final AtomicInteger j = new AtomicInteger(0);
+
+    private final LongAdder  k = new LongAdder();
+    public static void main(String[] args) throws InterruptedException {
         SynchronizedTest test = new SynchronizedTest();
-        for (int i = 0; i < 100; i++) {
+        var start1 = System.currentTimeMillis();
+        var count1 = new CountDownLatch(5000);
+        for (int i = 0; i < 5000; i++) {
             new Thread(() -> {
-                for (int j = 0; j < 100; j++) {
+                for (int j = 0; j < 100000; j++) {
                     test.add();
                 }
+                count1.countDown();
             }).start();
         }
-        while (Thread.activeCount() > 2) {
-            Thread.yield();
+        count1.await();
+        System.out.println(System.currentTimeMillis() - start1);
+        System.out.println(test.i);
+
+        var start2 = System.currentTimeMillis();
+        var count2 = new CountDownLatch(5000);
+        for (int i = 0; i < 5000; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 100000; j++) {
+                    test.j.incrementAndGet();
+                }
+                count2.countDown();
+            }).start();
         }
-        System.out.println(Thread.currentThread().getName() + ":" + test.i);
+        count2.await();
+        System.out.println(System.currentTimeMillis() - start2);
+        System.out.println(test.j.get());
+
+        var start3 = System.currentTimeMillis();
+        var count3 = new CountDownLatch(5000);
+        for (int i = 0; i < 5000; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 100000; j++) {
+                    test.k.add(1);
+                }
+                count3.countDown();
+            }).start();
+        }
+        count3.await();
+        System.out.println(System.currentTimeMillis() - start3);
+        System.out.println(test.k.sum());
     }
 }
